ae0da8dbc7ede302fdf6a8e120230f36ae1aee85
Thomas Stegmaier <thosteg@googlemail.com>
2024-10-28 00:21:49 +0100

    minimal tauri program

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,5 +1,18 @@
-mod helperlib;
+// Prevents additional console window on Windows in release, DO NOT REMOVE!!
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-fn main() {
-    println!("Hello, world!");
+fn run_tauri_gui() -> Result<(), tauri::Error> {
+    tauri::async_runtime::set(tokio::runtime::Handle::current());
+    let res = tauri::Builder::default().run(tauri::generate_context!());
+    res
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error + Send>> {
+    let res = run_tauri_gui();
+
+    match res {
+        Ok(_) => Ok(()),
+        Err(_) => Ok((/* for now */)),
+    }
 }
