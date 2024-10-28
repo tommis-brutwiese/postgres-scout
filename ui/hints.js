3ea0049f45caa7ab848b3def4ebb9a05eb64cb0f
Thomas Stegmaier <thosteg@googlemail.com>
2024-10-28 22:28:19 +0100

    javascript debugging hints

--- ui/hints.js
+++ ui/hints.js
@@ -7,3 +7,45 @@ let query = await invoke("suggest_query", {});
 invoke("db_query",{ query: query})
 .then((message) => {console.log("Message:" + JSON.stringify(message))})
 .catch((error) => {console.log("Error:" + JSON.stringify(error))});
+
+/* Query Types */
+
+// Query: backend request to database as defined by rust backend
+//        Examples: GetDatabases, GetTables, CustomQuery
+//
+//        For instance:
+//
+//        {
+//          CustomQuery: {
+//            database: database,
+//            sql_query: sqlQuery
+//          }
+//        }
+//
+//        or
+//
+//        "GetDatabases"
+//
+//        or
+//
+//        { "GetTables" : "mydatabase" }
+//
+// Path: [Database], [Path]
+//        Query can be created from this
+//
+// Connection: determines which database to connect to
+//        Currently is simply a string,
+//        contains (host, port, user, password)
+//
+//       {
+//         Stateless: connection_string
+//       }
+//
+// FullQuery: Query + Connection
+//
+//       {
+//         "connection": <some connection>,
+//         "query": <query>
+//       }
+//
+// Definitive: db.rs
